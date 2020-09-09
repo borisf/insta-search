e2bf385d68f6702698406eedb678e4e59a7fc387
Boris Farber <boris.farber@gmail.com>
2020-09-09 15:56:01 +0300

    package rename

--- src/main/java/com/borisfarber/instasearch/formats/Clazz.java
+++ src/main/java/com/borisfarber/instasearch/formats/Clazz.java
@@ -11,7 +11,7 @@
   * See the License for the specific language governing permissions and
   * limitations under the License.
   */
-package com.borisfarber.instasearch.binary;
+package com.borisfarber.instasearch.formats;
 
 import com.borisfarber.instasearch.controllers.PrivateFolder;
 import com.borisfarber.instasearch.controllers.Pair;
--- src/main/java/com/borisfarber/instasearch/formats/XmlDecompressor.java
+++ src/main/java/com/borisfarber/instasearch/formats/XmlDecompressor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.borisfarber.instasearch.binary;
+package com.borisfarber.instasearch.formats;
 
 import com.borisfarber.instasearch.controllers.PrivateFolder;
 import com.google.common.io.LittleEndianDataInputStream;
